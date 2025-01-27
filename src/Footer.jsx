87fbc16031de2291e38b4b6d2b38c1a117bd7877
onni82 <onni.bucht@me.com>
2025-01-27 18:51:08 +0100

    Updated formatting

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,10 +1,10 @@
 function Footer() {
-  return (
-	<footer id="footer">
-		<p>&copy; 2025 <span class="black-text" id="easteregg">Onni Bucht</span></p>
-		<a class="clickable contact" href="./contact">Contact</a>
-	</footer>
-  );
+	return (
+		<footer id="footer">
+			<p>&copy; 2025 <span class="black-text" id="easteregg">Onni Bucht</span></p>
+			<a class="clickable contact" href="./contact">Contact</a>
+		</footer>
+	);
 }
 
 export default Footer;
