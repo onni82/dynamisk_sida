af446e66922567156eb8fe2df0eceaad9b7c1695
onni82 <onni.bucht@me.com>
2025-01-27 18:44:47 +0100

    Added Header

---
+++ src/Header.jsx
@@ -0,0 +1,21 @@
+function Header() {
+  return (
+	<header>
+		<div>
+			<a href="./" aria-label="Home" id="header-logo"></a>
+			<span aria-label="Menu" id="menu-icon">&#9776;</span>
+		</div>
+		<nav id="menu">
+			<ul>
+				<li><a class="clickable index" href="./">Home</a></li>
+				<li><a class="clickable work" href="./work">Work Experience</a></li>
+				<li><a class="clickable coding" href="./coding">Coding</a></li>
+				<li><a class="clickable education" href="./education">Education</a></li>
+				<li><a class="clickable skills" href="./skills">Skills</a></li>
+			</ul>
+		</nav>
+	</header>
+  );
+}
+
+export default Header;
