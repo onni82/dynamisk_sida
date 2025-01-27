1680a497fe101bbaa230c8ede49ab34fad94c6b0
onni82 <onni.bucht@me.com>
2025-01-27 18:53:30 +0100

    Added new Date().getFullYear()

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,7 +1,7 @@
 function Footer() {
 	return (
 		<footer id="footer">
-			<p>&copy; 2025 <span class="black-text" id="easteregg">Onni Bucht</span></p>
+			<p>&copy; {new Date().getFullYear()} <span class="black-text" id="easteregg">Onni Bucht</span></p>
 			<a class="clickable contact" href="./contact">Contact</a>
 		</footer>
 	);
