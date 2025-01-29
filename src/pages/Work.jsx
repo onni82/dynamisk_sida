cf2a1dc44ba5f693b93bd392554ffed1d92626d1
onni82 <onni.bucht@me.com>
2025-01-29 18:40:41 +0100

    Updated Work

--- src/pages/Work.jsx
+++ src/pages/Work.jsx
@@ -3,6 +3,23 @@ import React from "react";
 const Work = () => {
 	return (
 		<main>
+			<h1>Work Experience</h1>
+			<div>
+				<section>
+					<p class="sub-category">Securitas</p>
+					<p>April 2022-Present - Security Guard</p>
+					<p>Full-time contract as a security guard. On behalf of Securitas, I patrol several buildings at one
+						of their clients' sites in Södertälje.<br/>This also includes report writing and getting called
+						out on fire and emergency alarms, as well as working as a receptionist and escort when needed.
+					</p>
+					<p>Södertälje, Sweden</p>
+				</section>
+				<section>
+					<p class="sub-category">Mind Proxy AB</p>
+					<p>June 2023-Sept 2023 - Supervisor and Administrator</p>
+					<p>Norberg, Sweden (Remote)</p>
+				</section>
+			</div>
 		</main>
 	);
 };
