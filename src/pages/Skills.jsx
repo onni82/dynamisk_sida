7e54d6cfa1f8097d366f62a7871b647d5e2b9559
onni82 <onni.bucht@me.com>
2025-01-29 18:40:08 +0100

    Updated Skills

--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -3,6 +3,36 @@ import React from "react";
 const Skills = () => {
 	return (
 		<main>
+			<h1>Skills</h1>
+			<div>
+				<section>
+					<p class="sub-category">Media Skills</p>
+					<p>Music writing, lyric writing, audio engineering &amp; music production</p>
+				</section>
+				<section>
+					<p class="sub-category">Languages</p>
+					<p>English, Swedish &amp; some Korean</p>
+				</section>
+			</div>
+			<h1>Tools</h1>
+			<div>
+				<section>
+					<p class="sub-category">Operating Systems</p>
+					<p>Windows, macOS, Ubuntu (Desktop &amp; Server)</p>
+				</section>
+				<section>
+					<p class="sub-category">Code Editors/IDE</p>
+					<p>Visual Studio, Sublime Text, Code::Blocks, Eclipse</p>
+				</section>
+				<section>
+					<p class="sub-category">Digital Audio Workstations</p>
+					<p>Avid Pro Tools, Image-Line FL Studio, Apple Logic Pro</p>
+				</section>
+				<section>
+					<p class="sub-category">Video Editing Software</p>
+					<p>Adobe Premiere Pro, Avid Media Composer, Apple Final Cut Pro</p>
+				</section>
+			</div>
 		</main>
 	);
 };
