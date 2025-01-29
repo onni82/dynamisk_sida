fb3d53a1b0eae239bf596529dd762a1002f16aae
onni82 <onni.bucht@me.com>
2025-01-29 12:19:16 +0100

    added commented out code

--- src/pages/Coding.jsx
+++ src/pages/Coding.jsx
@@ -1,6 +1,10 @@
 import React from "react";
+import { useEffect } from "react";
 
 const Coding = () => {
+	// useEffect(function() {
+	// 	document.title = "Coding";
+	// }, []);
 	return (
 		<main>
 		</main>
