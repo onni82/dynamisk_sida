59ed1bc75d9537411b2b05aceab262e2b934064a
onni82 <onni.bucht@me.com>
2025-01-29 18:55:15 +0100

    Changed Footer from function to const

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,10 @@
-function Footer() {
+const Footer = () => {
 	return (
 		<footer id="footer">
 			<p>&copy; {new Date().getFullYear()} <span class="black-text" id="easteregg">Onni Bucht</span></p>
 			<a class="clickable contact" href="/contact">Contact</a>
 		</footer>
 	);
-}
+};
 
 export default Footer;
