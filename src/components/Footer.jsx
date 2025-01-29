add2e2e8ddc67dc853b1249b85de26102909c36b
onni82 <onni.bucht@me.com>
2025-01-29 11:25:42 +0100

    updated href in footer and header

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ function Footer() {
 	return (
 		<footer id="footer">
 			<p>&copy; {new Date().getFullYear()} <span class="black-text" id="easteregg">Onni Bucht</span></p>
-			<a class="clickable contact" href="./contact">Contact</a>
+			<a class="clickable contact" href="/contact">Contact</a>
 		</footer>
 	);
 }
