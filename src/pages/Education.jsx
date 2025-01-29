5268ce1e511025557350afb758ff3f7ae3e67fb7
onni82 <onni.bucht@me.com>
2025-01-29 16:14:05 +0100

    Updated Home and Education

--- src/pages/Education.jsx
+++ src/pages/Education.jsx
@@ -3,6 +3,46 @@ import React from "react";
 const Education = () => {
 	return (
 		<main>
+			<h1>Education</h1>
+			<div>
+				<section>
+					<p class="sub-category">Iris Hadar</p>
+					<p>Apr 2024-Present - Municipal Adult Education - Web development 1</p>
+					<p>Stockholm/Södertälje, Sweden (online course)</p>
+				</section>
+				<section>
+					<p class="sub-category">Iris Hadar</p>
+					<p>Nov 2023-Jan 2024 - Municipal Adult Education - Mathematics 2a</p>
+					<p>Stockholm/Södertälje, Sweden (online course)</p>
+				</section>
+				<section>
+					<p class="sub-category">Iris Hadar</p>
+					<p>July 2023-Sep 2023 - Municipal Adult Education - Programming 1 (C++)</p>
+					<p>Stockholm/Södertälje, Sweden (online course)</p>
+				</section>
+				<section>
+					<p class="sub-category">Be Licensed</p>
+					<p>Feb 2022-Mar 2022 - Vocational Training - Video Production</p>
+					<p>Stockholm, Sweden (online course)</p>
+				</section>
+				<section>
+					<p class="sub-category">Be Licensed</p>
+					<p>Dec 2021-Jan 2022 - Vocational Training - Sound Production</p>
+					<p>Stockholm, Sweden (online course)</p>
+				</section>
+				<section>
+					<p class="sub-category">Dalarna University</p>
+					<p>2018-2021 - Bachelor of Science - Sound and Music Production</p>
+					<p>Falun, Sweden</p>
+				</section>
+			</div>
+			<h1>Certifications</h1>
+			<div>
+				<section>
+					<p class="sub-category">Avid Pro Tools</p>
+					<p>Certified User - version 2020 and version 12.8</p>
+				</section>
+			</div>
 		</main>
 	);
 };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,26 @@
 import React from "react";
+import squareprofile from "../images/squareprofile.jpg";
 
 const Home = () => {
 	return (
 		<main>
+			<h1>This is Onni Bucht</h1>
+			<div>
+				<section>
+					<img class="fit-image" src="{squareprofile}" alt="Profile picture of Onni Bucht" />
+				</section>
+				<section>
+					<p>Works as a <span class="bold-text">security guard</span>.</p>
+					<p>Studying <span class="bold-text">.NET</span> full stack web development.</p>
+					<p>Self-taught in <span class="bold-text">HTML</span>, <span class="bold-text">JavaScript</span>,
+						<span class="bold-text">SQL</span>, <span class="bold-text">PHP</span> and
+						<span class="bold-text">Java</span>.</p>
+					<p>Has a bachelor's degree in <span class="bold-text">sound and music production</span>.</p>
+					<p>Took a vocational course in <span class="bold-text">video production</span>.</p>
+					<p>Have taken courses in <span class="bold-text">C++</span>, <span class="bold-text">Japanese</span>
+						and <span class="bold-text">Korean</span>.</p>
+				</section>
+			</div>
 		</main>
 	);
 };
