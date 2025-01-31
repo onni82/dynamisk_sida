a91d5fedd7fc81502fdf52432aecaf1929914070
onni82 <onni.bucht@me.com>
2025-01-31 19:35:08 +0100

    Changed languageIcons from js to jsx and imported icons in Coding.jsx

--- src/pages/Coding.jsx
+++ src/pages/Coding.jsx
@@ -1,4 +1,8 @@
 import React, { useState, useEffect } from "react";
+import { FaJs, FaPython, FaJava, FaReact, FaNodeJs, FaPhp, FaRust, FaSwift, FaCuttlefish, FaHtml5, FaCss3 } from "react-icons/fa";
+import { SiCplusplus, SiGo, SiKotlin, SiRuby, SiTypescript } from "react-icons/si";
+import { TbBrandCSharp } from "react-icons/tb";
+import languageIcons from "../utils/languageIcons";
 
 const Coding = () => {
 	// useEffect(function() {
@@ -35,6 +39,7 @@ const Coding = () => {
 				{repos.map((repo) => (
 					<section key={repo.id}>
 						<p className="sub-category">{repo.name}</p>
+						{(languageIcons[repo.language] || (() => <FaJs className="lang-icon default" />))()}
 						<p>Written in {repo.language || "unknown language"}.</p>
 						<p>{repo.description || "No description available"}.<br/><a href={repo.html_url} target="_blank" rel="noopener noreferrer">GitHub repository link</a>.</p>
 					</section>
