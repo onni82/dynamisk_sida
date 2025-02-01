7bbdf4698e3c395ad39c5438038a866446a94237
onni82 <onni.bucht@me.com>
2025-02-01 13:12:46 +0100

    Updated page not found text

--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -1,17 +1,15 @@
 import React from "react";
-import squareprofile from "../images/squareprofile.jpg";
 
 const PageNotFound = () => {
 	return (
 		<main>
-			<h1>⚔️ 404: Quest Failed! ⚔️</h1>
+			<h1>Page Not Found</h1>
 			<div>
 				<section>
 					<p>Adventurer, you have ventured into the unknown... but alas, this page does not exist!</p>
-					<div class="options">
-						<a href="/">🏰 Return to the Guild Hall</a>
-						<a href="#" onclick="history.back()">📜 Search the Ancient Scrolls</a>
-						<a href="/contact">🔮 Consult the Oracle</a>
+					<div className="options">
+						<a href="/">Return to Home</a>
+						<a href="/contact">Contact</a>
 					</div>
 				</section>
 			</div>
