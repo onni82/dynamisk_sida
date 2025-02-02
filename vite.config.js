60145f194238c64cd4fabeea2ece307c74f4f902
onni82 <onni.bucht@me.com>
2025-02-02 15:28:42 +0100

    Updated base and homepage path

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-	base: "/dynamisk_sida",
-	plugins: [react()]
+	plugins: [react()],
+	base: "/dynamisk_sida/"
 })
