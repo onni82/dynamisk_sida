44db862b10f9c3ec75328bc67fc00cbbe502be5f
onni82 <onni.bucht@me.com>
2025-02-02 22:01:19 +0100

    Imported ViteSSG and BrowserRouter and converted to statically generate sites

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,22 @@
+import { ViteSSG } from 'vite-ssg';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import './styles/global.css';
 import './styles/print.css';
 import App from './App.jsx';
 
-createRoot(document.getElementById('box-content')).render(
-	<StrictMode>
-		<App />
-	</StrictMode>,
-)
+// createRoot(document.getElementById('box-content')).render(
+// 	<StrictMode>
+// 		<App />
+// 	</StrictMode>,
+// )
+
+export const createApp = ViteSSG(
+	() => (
+		<BrowserRouter>
+			<App />
+		</BrowserRouter>
+	),
+	{ base: '/dynamisk_sida/' }
+);
