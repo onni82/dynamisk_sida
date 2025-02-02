7bbf5dac1f4cfe45ad2daa805a3bb9769e48ed52
onni82 <onni.bucht@me.com>
2025-02-02 21:49:00 +0100

    Imported ViteSSG

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { ViteSSG } from 'vite-ssg';
 
 // https://vite.dev/config/
 export default defineConfig(({ command, mode, isSsrBuild, isPreview }) => {
 	if (command === 'serve') {
 		return {
 			// dev specific config
-			plugins: [react()],
+			plugins: [react(), ViteSSG()],
 		}
 	} else {
 		// command === 'build'
 		return {
 			//build specific config
-			plugins: [react()],
+			plugins: [react(), ViteSSG()],
 			base: '/dynamisk_sida/',
 		}
 	}
-})
+});
