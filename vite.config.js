405c453eb6d6332df8fbdc1c781a217fdf4f0aa4
onni82 <onni.bucht@me.com>
2025-02-02 15:48:27 +0100

    Only includes base during production and not during development

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,18 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-	plugins: [react()],
-	base: "/dynamisk_sida/"
+export default defineConfig(({ command, mode, isSsrBuild, isPreview }) => {
+	if (command === 'serve') {
+		return {
+			// dev specific config
+			plugins: [react()],
+		}
+	} else {
+		// command === 'build'
+		return {
+			//build specific config
+			plugins: [react()],
+			base: "/dynamisk_sida/"
+		}
+	}
 })
