b34c8b903f677a0cdccaf71e7091271c1b2d9bed
onni82 <onni.bucht@me.com>
2025-02-03 15:37:20 +0100

    Added 404 and main rollupOptions

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import resolve from 'path';
 
 // https://vite.dev/config/
 export default defineConfig(({ command, mode, isSsrBuild, isPreview }) => {
@@ -14,6 +15,14 @@ export default defineConfig(({ command, mode, isSsrBuild, isPreview }) => {
 			//build specific config
 			plugins: [react()],
 			base: '/dynamisk_sida/',
+			build: {
+				rollupOptions: {
+					input: {
+						main: resolve(__dirname, "index.html"),
+						404:  resolve(__dirname, "public/404.html"),
+					}
+				}
+			}
 		}
 	}
 });
