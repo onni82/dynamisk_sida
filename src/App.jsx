85481ea133e4ec9cad51b1d67335814f68117567
onni82 <onni.bucht@me.com>
2025-02-03 13:22:58 +0100

    Added Router basename

--- src/App.jsx
+++ src/App.jsx
@@ -15,16 +15,16 @@ const App = () => {
 	return (
 		<>
 			<EasterEggModal />
-			<Router>
+			<Router basename="/dynamisk_sida">
 				<Header />
 				<Routes>
-					<Route path="/dynamisk_sida/" element={<Home />} />
-					<Route path="/dynamisk_sida/coding" element={<Coding />} />
-					<Route path="/dynamisk_sida/contact" element={<Contact />} />
-					<Route path="/dynamisk_sida/education" element={<Education />} />
-					<Route path="/dynamisk_sida/skills" element={<Skills />} />
-					<Route path="/dynamisk_sida/work" element={<Work />} />
-					<Route path="/dynamisk_sida/*" element={<PageNotFound />} />
+					<Route path="/" element={<Home />} />
+					<Route path="/coding" element={<Coding />} />
+					<Route path="/contact" element={<Contact />} />
+					<Route path="/education" element={<Education />} />
+					<Route path="/skills" element={<Skills />} />
+					<Route path="/work" element={<Work />} />
+					<Route path="/*" element={<PageNotFound />} />
 				</Routes>
 				<Footer />
 			</Router>
