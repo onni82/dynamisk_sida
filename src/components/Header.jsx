4a8308ab33e96485e63d230cc6a32c7039bf2f37
onni82 <onni.bucht@me.com>
2025-02-09 00:23:29 +0100

    Added onClick

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { useLocation } from "react-router-dom";
 import { Link } from "react-router-dom";
 
@@ -9,24 +10,24 @@ const Header = ({ menuOpen, setMenuOpen, menuClose }) => {
 		<header>
 			<div>
 				<Link to="/" aria-label="Home" id="header-logo"></Link>
-				<span aria-label="Menu" id="menu-icon">&#9776;</span>
+				<span aria-label="Menu" id="menu-icon" onClick={toggleMenu} className={menuOpen ? "open" : ""}>&#9776;</span>
 			</div>
-			<nav id="menu">
+			<nav id="menu" className={menuOpen ? "open" : ""}>
 				<ul>
 					<li>
-						<Link to="/" aria-current={location.pathname === "/" ? "page" : undefined}>Home</Link>
+						<Link to="/" aria-current={location.pathname === "/" ? "page" : undefined} onClick={menuClose}>Home</Link>
 					</li>
 					<li>
-						<Link to="/work" aria-current={location.pathname === "/work" ? "page" : undefined}>Work</Link>
+						<Link to="/work" aria-current={location.pathname === "/work" ? "page" : undefined} onClick={menuClose}>Work</Link>
 					</li>
 					<li>
-						<Link to="/coding" aria-current={location.pathname === "/coding" ? "page" : undefined}>Coding</Link>
+						<Link to="/coding" aria-current={location.pathname === "/coding" ? "page" : undefined} onClick={menuClose}>Coding</Link>
 					</li>
 					<li>
-						<Link to="/education" aria-current={location.pathname === "/education" ? "page" : undefined}>Education</Link>
+						<Link to="/education" aria-current={location.pathname === "/education" ? "page" : undefined} onClick={menuClose}>Education</Link>
 					</li>
 					<li>
-						<Link to="/skills" aria-current={location.pathname === "/skills" ? "page" : undefined}>Skills</Link>
+						<Link to="/skills" aria-current={location.pathname === "/skills" ? "page" : undefined} onClick={menuClose}>Skills</Link>
 					</li>
 				</ul>
 			</nav>
